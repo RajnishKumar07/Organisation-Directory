4269a036fbd72ae707ad0f9430337373721fea8f
Rajnish Kumar <rajnish6633@gmail.com>
2026-08-30 11:23:00 +0530

    feat: organisation list component created with route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/organisation',
+    pathMatch: 'full',
+  },
+  {
+    path: 'organisation',
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/organisation/list/list').then((c) => c.List),
+      },
+    ],
+  },
+];
